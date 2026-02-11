8fed4f434e420c35d4372934f715f6a9a16a8dfc
Uk-Cat <anthonyleng@hotmail.com>
2026-02-11 22:48:07 +0000

    yo

--- Login/GoogleLogin.js
+++ Login/GoogleLogin.js
@@ -6,16 +6,16 @@ async function handleCredentialResponse(response) {
     
     // Save to Supabase
     const result = await saveUserToSupabase(userObject);
-    
     if (result.success) {
         // Save user to localStorage to remember login
         localStorage.setItem('vividmind_user', JSON.stringify(userObject));
-        
         alert("Logged in!\nName: " + userObject.name + "\nEmail: " + userObject.email);
-        
-        // Redirect to main page
-        window.location.href = '../index.html';
+        // Redirect to main page only after localStorage is set
+        setTimeout(() => {
+            window.location.href = '../index.html';
+        }, 200);
     } else {
+        // Do not redirect if Supabase save fails
         alert("Login successful but couldn't save to database: " + result.error.message);
     }
 }
